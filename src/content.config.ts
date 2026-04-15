2474af8b1f202d1b527f951dc2e4697671587b04
gasser mohamed <gasserm525@gmail.com>
2026-04-15 18:23:38 +0200

    Astro branch edits and refactorization  (#3)
    Co-authored-by: Zero <188656344+k5602@users.noreply.github.com>

---
+++ src/content.config.ts
@@ -0,0 +1,28 @@
+import { defineCollection } from "astro:content";
+import { docsLoader } from "@astrojs/starlight/loaders";
+import { docsSchema } from "@astrojs/starlight/schema";
+import { z } from "zod";
+
+const FormattSchema = z.object({
+  title: z.string(),
+  date: z.coerce.date(),
+  description: z.string(),
+  subject: z.string(),
+  level: z.enum(["1", "2", "3", "4", "5"]),
+  term: z.number().min(1).max(2),
+  prof: z.string(),
+  contributor: z.string(),
+  tags: z.array(z.string()),
+  language: z.enum(["ar", "en"]),
+})
+
+export type Formatt = z.infer<typeof FormattSchema>;
+
+const docs = defineCollection({
+  loader: docsLoader(),
+  schema: docsSchema({ extend: FormattSchema }),
+});
+
+export const collections = {
+  docs,
+};
